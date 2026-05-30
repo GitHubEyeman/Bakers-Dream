adb5a274aee6898a0a05e9fc4381b2ced40d3af4
sleepyeoul <sleepygwizard123@gmail.com>
2026-05-30 20:11:27 +0800

    Update IngrePrep.cs

--- Assets/1. Mixing Feature/Scripts/IngrePrep.cs
+++ Assets/1. Mixing Feature/Scripts/IngrePrep.cs
@@ -14,7 +14,8 @@ public class IngrePrep : MonoBehaviour
         Normal,
         Whisk,
         Scoop,
-        Cup
+        Cup,
+        Scale,
     }
 
     // change ScoopMeasure
@@ -53,31 +54,44 @@ public class IngrePrep : MonoBehaviour
 
     public float CupMeasurement;
     public MouseType currentMouseType = MouseType.Normal;
-    public ScoopMeasure currentScoop = ScoopMeasure.FullScoop;
-    public CupMeasure currentCup = CupMeasure.FullCup;
+    public ScoopMeasure currentScoop = ScoopMeasure.FullScoops;
+    public CupMeasure currentCup = CupMeasure.Fullcups;
     public IngredientType currentIngredient = IngredientType.None;
 
     void Start()
     {
-        CupRuler.onValueChanged.AddListener((value) =>
+        if (CupRuler != null)
         {
-            if (value == 1f)
-                print("Cup measure set to 50ml");
+            CupRuler.onValueChanged.AddListener((value) =>
+            {
+                if (value == 1f)
+                {
+                    print("Cup measure set to 50ml");
                     currentCup = CupMeasure.OneFifthcups;
-            else if (value == 2f)
-                print($"Cup measure set to 100m1");
+                }
+                else if (value == 2f)
+                {
+                    print("Cup measure set to 100ml");
                     currentCup = CupMeasure.Quartercups;
-            else if (value == 3f)
-                print($"Cup measure set to 150ml");
+                }
+                else if (value == 3f)
+                {
+                    print("Cup measure set to 150ml");
                     currentCup = CupMeasure.OneThirdcups;
-            else if (value == 4f)
-                print($"Cup measure set to 200ml");
-                    currentCup = CupMeasure.Halfcups,;
-            else if (value == 5f)
-                print($"Cup measure set to 250ml");
-                    currentCup = CupMeasure.FullCup;
-            // Optionally update UI or do something with the new cupMeasure value
-        });
+                }
+                else if (value == 4f)
+                {
+                    print("Cup measure set to 200ml");
+                    currentCup = CupMeasure.Halfcups;
+                }
+                else if (value == 5f)
+                {
+                    print("Cup measure set to 250ml");
+                    currentCup = CupMeasure.Fullcups;
+                }
+                // Optionally update UI or do something with the new cupMeasure value
+            });
+        }
     }
 
     // Update is called once per frame
@@ -110,9 +124,140 @@ public class IngrePrep : MonoBehaviour
         print("Mouse type set to Normal");
     }
 
+    public void ScaleMode()
+    {
+        currentMouseType = MouseType.Scale;
+        print("Mouse type set to Scale");
+    }
+
+    // Helper to validate enum index
+    private bool IsValidEnumIndex(System.Type enumType, int index)
+    {
+        if (!enumType.IsEnum) return false;
+        return index >= 0 && index < System.Enum.GetValues(enumType).Length;
+    }
+
+    // Scoop setters: single validated method for UI buttons (pass 0..4)
     public void SetScoopMeasure(int measure)
     {
+        if (!IsValidEnumIndex(typeof(ScoopMeasure), measure))
+        {
+            print($"Invalid scoop measure index: {measure}");
+            return;
+        }
+
         currentScoop = (ScoopMeasure)measure;
         print($"Scoop measure set to: {currentScoop}");
+    }
+
+    // Ingredient setter: single validated method for UI buttons (pass 1..n; 0 is None)
+    public void SetIngredientWater()
+    {
+        if (currentMouseType == MouseType.Cup)
+        {
+            currentIngredient = IngredientType.Water;
+            print("Ingredient set to: Water");
+        }
+        else
+            print("Water can only be set in Cup mode");
+    }
+
+    public void SetIngredientOliveOil()
+    {
+        if (currentMouseType == MouseType.Cup)
+        {
+            currentIngredient = IngredientType.OliveOil;
+            print("Ingredient set to: OliveOil");
+        }
+        else
+            print("Olive Oil can only be set in Cup mode");
+    }
+
+    public void SetIngredientFlour()
+    {
+        if (currentMouseType == MouseType.Scoop
+        {
+            currentIngredient = IngredientType.Flour;
+            print("Ingredient set to: Flour");
+        }
+        else
+            print("Flour can only be set in Scoop mode");
+    }
+
+    public void SetIngredientYeast()
+    {
+        if (currentMouseType == MouseType.Scoop)
+        {
+            currentIngredient = IngredientType.Yeast;
+            print("Ingredient set to: Yeast");
+        }
+        else
+            print("Yeast can only be set in Scoop mode");
+    }
+
+    public void SetIngredientSalt()
+    {
+        if (currentMouseType == MouseType.Scoop)
+        {
+            currentIngredient = IngredientType.Salt;
+            print("Ingredient set to: Salt");
+        }
+        else
+            print("Salt can only be set in Scoop mode");
+    }
+
+    public void SetIngredientSugar()
+    {
+        if (currentMouseType == MouseType.Scoop)
+        {
+            currentIngredient = IngredientType.Sugar;
+            print("Ingredient set to: Sugar");
+        }
+        else
+            print("Sugar can only be set in Scoop mode");
+    }
+
+    public void SetIngredientHoney()
+    {
+        if (currentMouseType == MouseType.Cup)
+        {
+            currentIngredient = IngredientType.Honey;
+            print("Ingredient set to: Honey");
+        }
+        else
+            print("Honey can only be set in Cup mode");
+    }
+
+    public void SetIngredientButter()
+    {
+        if (currentMouseType == MouseType.Scale)
+        {
+            currentIngredient = IngredientType.Butter;
+            print("Ingredient set to: Butter");
+        }
+        else
+            print("Butter can only be set in Scale mode");
+    }
+
+    public void SetIngredientMilk()
+    {
+        if (currentMouseType == MouseType.Cup)
+        {
+            currentIngredient = IngredientType.Milk;
+            print("Ingredient set to: Milk");
+        }
+        else
+            print("Milk can only be set in Cup mode");
+    }
+
+    public void SetIngredientEggs()
+    {
+        if (currentMouseType == MouseType.Normal)
+        {
+            currentIngredient = IngredientType.Eggs;
+            print("Ingredient set to: Eggs");
+        }
+        else
+            print("Eggs can only be set in Normal mode");
     }
 }
