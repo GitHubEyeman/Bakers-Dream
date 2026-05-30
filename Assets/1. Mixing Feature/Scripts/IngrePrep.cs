dbc936f241256306fb07eb13c41add0e03e89e56
sleepyeoul <sleepygwizard123@gmail.com>
2026-05-30 17:03:14 +0800

    Update IngrePrep.cs

--- Assets/1. Mixing Feature/Scripts/IngrePrep.cs
+++ Assets/1. Mixing Feature/Scripts/IngrePrep.cs
@@ -20,24 +20,62 @@ public class IngrePrep : MonoBehaviour
     // change ScoopMeasure
     public enum ScoopMeasure
     {
-        QuarterScoop,
-        HalfScoop,
-        FullScoop,
-        HeapingScoop,
-        Tablespoon,
-        Teaspoon,
-        Custom
+        FullScoops,
+        HalfScoops,
+        OneThirdScoops,
+        QuarterScoops,
+        OneFifthScoops,
     }
+
+    public enum CupMeasure
+    {
+        Fullcups,
+        Halfcups,
+        OneThirdcups,
+        Quartercups,
+        OneFifthcups,
+    }
+
+    public enum IngredientType
+    {
+        None,
+        Water,
+        OliveOil,
+        Flour,
+        Yeast,
+        Salt,
+        Sugar,
+        Honey,
+        Butter,
+        Milk,
+        Eggs,
+    }
+
     public float CupMeasurement;
     public MouseType currentMouseType = MouseType.Normal;
     public ScoopMeasure currentScoop = ScoopMeasure.FullScoop;
+    public CupMeasure currentCup = CupMeasure.FullCup;
+    public IngredientType currentIngredient = IngredientType.None;
 
     void Start()
     {
         CupRuler.onValueChanged.AddListener((value) =>
         {
-            CupMeasurement = value;
-            print($"Cup measure updated to: {CupMeasurement} cups");
+            if (value == 1f)
+                print("Cup measure set to 50ml");
+                    currentCup = CupMeasure.OneFifthcups;
+            else if (value == 2f)
+                print($"Cup measure set to 100m1");
+                    currentCup = CupMeasure.Quartercups;
+            else if (value == 3f)
+                print($"Cup measure set to 150ml");
+                    currentCup = CupMeasure.OneThirdcups;
+            else if (value == 4f)
+                print($"Cup measure set to 200ml");
+                    currentCup = CupMeasure.Halfcups,;
+            else if (value == 5f)
+                print($"Cup measure set to 250ml");
+                    currentCup = CupMeasure.FullCup;
             // Optionally update UI or do something with the new cupMeasure value
         });
     }
@@ -46,6 +84,12 @@ public class IngrePrep : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void WhiskMode()
+    {
+        currentMouseType = MouseType.Whisk;
+        print("Mouse type set to Whisk");
     }
 
     public void ScoopMode()
@@ -58,5 +102,17 @@ public class IngrePrep : MonoBehaviour
     {
         currentMouseType = MouseType.Cup;
         print("Mouse type set to Cup");
+    }
+
+    public void NormalMode()
+    {
+        currentMouseType = MouseType.Normal;
+        print("Mouse type set to Normal");
+    }
+
+    public void SetScoopMeasure(int measure)
+    {
+        currentScoop = (ScoopMeasure)measure;
+        print($"Scoop measure set to: {currentScoop}");
     }
 }
