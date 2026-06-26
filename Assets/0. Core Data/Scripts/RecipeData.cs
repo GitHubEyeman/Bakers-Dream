786229d94a7824100da357d3f519d7ebaff5452f
GitHubEyeman <aimansaidin@gmail.com>
2026-06-26 19:18:23 +0800

    Added Recipe Data

--- Assets/0. Core Data/Scripts/RecipeData.cs
+++ Assets/0. Core Data/Scripts/RecipeData.cs
@@ -19,6 +19,11 @@ public class RecipeData : ScriptableObject
         Milk,
         Eggs,
         Dough,
+        GDough,
+        UnderCookedDough,
+        FlatBrickDough,
+        CavedInDough,
+        OverCookedDough,
     }
 
     [Serializable]
